fe5faf69b73a290ac1a4e225beee91183085bd65
Benjamin Yolken <yolken@stripe.com>
2018-03-01 11:27:19 -0800

    Remove clevels.SendSystemAusterityLevel call from main

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/stripe/unilog/clevels"
 	"github.com/stripe/unilog/filters"
 	"github.com/stripe/unilog/logger"
 )
 
 func main() {
-	go clevels.SendSystemAusterityLevel()
-
 	u := &logger.Unilog{
 		Filters: []logger.Filter{
 			logger.Filter(filters.AusterityFilter),
