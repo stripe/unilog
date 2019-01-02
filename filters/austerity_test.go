0f3a5caea852cadb6467956bfdaace8997143122
Andreas Fuchs <asf@stripe.com>
2019-01-02 16:35:04 +0100

    Add a test for JSON austerity

--- filters/austerity_test.go
+++ filters/austerity_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stripe/unilog/clevels"
+	"github.com/stripe/unilog/json"
 )
 
 func TestAusterityFilter(t *testing.T) {
@@ -54,5 +55,39 @@ func TestAusterityFilter(t *testing.T) {
 }
 
 func TestAusterityJSON(t *testing.T) {
+	// Make sure SendSystemAusterityLevel is called before we override
+	// the underlying channel below
+	a := AusterityFilter{}
+	a.Setup(true)
+	clevels.SystemAusterityLevel = make(chan clevels.AusterityLevel)
+	kill := make(chan struct{})
 
+	go func() {
+		for {
+			select {
+			case clevels.SystemAusterityLevel <- clevels.Critical:
+			case <-kill:
+				return
+			}
+		}
+	}()
+
+	// seed rand deterministically
+	rand.Seed(17)
+
+	// count number of lines dropped
+	dropped := 0
+
+	// now sample out the line a bunch!
+	for i := 0; i < 10000; i++ {
+		line := json.LogLine{"message": "some random log line!", "clevel": "sheddableplus"}
+		a.FilterJSON(&line)
+		if _, ok := line["message"]; !ok {
+			dropped++
+		}
+	}
+
+	// this number is deterministic because rand is seeded & deterministic
+	assert.Equal(t, 8983, dropped)
+	kill <- struct{}{}
 }
